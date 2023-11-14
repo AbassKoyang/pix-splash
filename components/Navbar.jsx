8bf78786c819dd205ad4ecb7479b260b121c8f6c
Abass Koyang <abasskoyang12345@gmail.com>
2023-11-15 01:38:17 +0930

    adjusted ui based on user session

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -25,8 +25,6 @@ useEffect(()=>{
     const setUpProviders = async () =>{
         const response = await getProviders();
         setProvider(response);
-        console.log(response)
-        console.log(process.env.GOOGLE_CLIENT_ID)
     }
     setUpProviders();
 },[]);
@@ -72,18 +70,59 @@ const handleSearch = () => {
                 PS
             </Link>
 
-            <div className="w-full max-w-[20rem] md:max-w-2xl flex justify-between items-center p-2 pl-3 lg:p-3 bg-gray-200 lg:bg-gray-300 rounded-full lg:rounded-lg">
+            <div className="w-full max-w-[22rem] md:max-w-2xl flex justify-between items-center p-2 pl-3 lg:p-3 bg-gray-200 lg:bg-gray-300 rounded-full lg:rounded-lg">
                 <BsImages className='w-5 h-5 hidden md:block'/>
                 <input type="text" placeholder="Search for photos..." required className="w-[70%] h-full outline-none border-none stroke-none text-gray-700 text-sm md:text-lg bg-transparent" value={query} onKeyDown={handlePress} onChange={(e) => {handleQueryChange(e)}}/>
                 <button onClick={handleSearch}><BiSearch className='w-6 h-6 px-1 py-1 rounded-full bg-black text-white'/></button>
             </div>
         </div>
+
+
+        {session?.user ? (
+
+                <Image src={session.user.image} alt='Profile Image' width={50} height={50} className='object-contain rounded-full'/>
+
+            ) : (
+
+                   <>
+                    { provider &&
+
+                        Object.values(provider).map((providerTwo) =>(
+                            <button 
+                            type="button"
+                            key={providerTwo.name}
+                            onClick={() => signIn(providerTwo.id)}
+                            className='px-4 py-3 rounded-md bg-black text-white text-lg font-normal hidden lg:block'
+                            >
+                            Sign In
+                            </button>
+                        ))       
+                }
+                   </>
+
+            )}
+            
+            {/* Mobile toggle button */}
         
-        {session && (
-            <Image src={session.user.image} alt='Profile Image' width={70} height={70} className='object-contain'/>
-        )}
-        <button className='relative block lg:hidden transition-all duration-300' onClick={() => {setMenuToggle(!menuToggle), handleClick}}>
-            {!menuToggle ? (<FiMenu className='w-6 h-6 transition-all duration-300'/>) : (<FiX className='w-6 h-6 transition-all duration-300'/>)}
+        {session?.user ? (
+                    <button className='relative block lg:hidden transition-all duration-300' onClick={() => {setMenuToggle(!menuToggle), handleClick}}>
+                    {!menuToggle ? (<FiMenu className='w-8 h-8 transition-all duration-300'/>) : (<FiX className='w-8 h-8 transition-all duration-300'/>)}
+                    {menuToggle && (
+                        <motion.div
+                        initial={{ x: '100%', opacity: 0 }}
+                        animate={isAnimating ? controls : { x: '0', opacity: 1 }}
+                        transition={{ duration: 0.7 }}
+                        className="w-[170px] absolute flex flex-col gap-3 items-center justify-center top-14 right-1 bg-white shadow-lg p-5 rounded-md"
+                    >
+                        <Link href='/' className='text-gray-700 font-normal text-sm'>Profile</Link>
+                        <Link href='/' className='text-gray-700 font-normal text-sm'>Favourites</Link>
+                        <Link href='/' className='text-gray-700 font-normal text-sm'>Saved</Link>
+                    </motion.div>
+                    )}
+                </button>
+        ) : (
+            <button className='relative block lg:hidden transition-all duration-300' onClick={() => {setMenuToggle(!menuToggle), handleClick}}>
+            {!menuToggle ? (<FiMenu className='w-8 h-8 transition-all duration-300'/>) : (<FiX className='w-8 h-8 transition-all duration-300'/>)}
             {menuToggle && (
                 <motion.div
                 initial={{ x: '100%', opacity: 0 }}
@@ -91,28 +130,21 @@ const handleSearch = () => {
                 transition={{ duration: 0.7 }}
                 className="w-[170px] absolute flex flex-col gap-3 items-center justify-center top-14 right-1 bg-white shadow-lg p-5 rounded-md"
             >
-                <Link href='/' className='text-gray-700 font-normal text-sm'>Profile</Link>
-                <Link href='/' className='text-gray-700 font-normal text-sm'>Favourites</Link>
-                <Link href='/' className='text-gray-700 font-normal text-sm'>Saved</Link>
-                <button className='px-4 py-3 bg-black text-white text-sm text-medium rounded-full mt-2'>Sign Out</button>
+                { provider &&
+
+                        Object.values(provider).map((providerTwo) =>(
+                            <button className='px-4 py-3 bg-black text-white text-sm text-medium rounded-full mt-2' 
+                            type='button'
+                            key={providerTwo.name}
+                            onClick={()=> signIn(providerTwo.id)}>
+                                Sign In/Up
+                            </button>
+                        ))       
+                }
             </motion.div>
             )}
         </button>
-
-        {provider &&
-
-                Object.values(provider).map((providerTwo) =>(
-                    <button 
-                    type="button"
-                    key={providerTwo.name}
-                    onClick={() => signIn(providerTwo.id)}
-                    className='px-4 py-3 rounded-md bg-black text-white text-lg font-normal hidden lg:block'
-                    >
-                     Sign In
-                    </button>
-                ))
-                
-        }
+        )}
     </nav>
   )
 };
