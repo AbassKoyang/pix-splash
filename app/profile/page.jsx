aec7bf5099c1e6ff295e7dbd44fb36d967543516
Abass Koyang <abasskoyang12345@gmail.com>
2023-11-21 06:51:49 +0930

    resolved import conflict in profile page

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -1,6 +1,6 @@
 "use client"
 import React from 'react';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Navbar from '@/components/Navbar';
 import { useRouter } from 'next/navigation';
 import { useSession } from 'next-auth/react';
