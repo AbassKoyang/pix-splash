78a65fd5185165aaeb55c09fbb5e016b8c5aed95
Abass Koyang <abasskoyang12345@gmail.com>
2023-12-05 03:15:29 +0930

    worked on unsplash user profile page

--- app/unsplash-user/[id]/page.jsx
+++ app/unsplash-user/[id]/page.jsx
@@ -1,22 +1,42 @@
 "use client"
 import React, { useEffect, useState } from 'react'
 import { fetchUnsplashUserProfile } from '@/utils'
-import { useRouter } from 'next/navigation';
+import { useRouter, usePathname } from 'next/navigation';
 import { AiOutlineLoading3Quarters } from 'react-icons/ai';
 import UnsplashUserProfile from '@/components/UnsplashUserProfile';
+import Navbar from '@/components/Navbar';
+import { useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
+import toast from 'react-hot-toast';
 
 const UnslashUserProfile = () => {
     const [user, setUser] = useState(null);
     const [loading, setLoading] = useState(false);
     const router = useRouter();
-    const { id } = router?.query || {};
+    const dispatch = useDispatch();
+    const [pagination, setPagination] = useState(1);
+    const searchQuery = useSelector((state) => state.search.query);
+    const pathName = usePathname();
+    const username = pathName.toString().slice(15, pathName.length);
+
+
+    const handleSearch = async () => {
+        const newImages = await fetchImages(searchQuery, pagination);
+        if(newImages.length > 0){
+          dispatch(setImages(newImages));
+          router.push('/')
+        } else {
+          toast.error("Oops! No result.")
+        }
+      };
 
     useEffect(() => {
     const fetchUser = async () => {
             setLoading(true)
             try {
-                const data =  await fetchUnsplashUserProfile("efekurnaz");
+                const data =  await fetchUnsplashUserProfile(username);
                 console.log('data', data);
+                console.log(pathName)
                 setUser(data);
             } catch (error) {
                 console.error('Error fetching Unsplash user profile:', error);
@@ -30,6 +50,7 @@ const UnslashUserProfile = () => {
     
   return (
     <section className='w-full'>
+        <Navbar isSearchAllowed={true} otherStyles={`${'static'}`} onSearch={handleSearch}/>
         {loading ? (
         <p>Loading... <AiOutlineLoading3Quarters/></p>
       ) : user ? (
--- components/UnsplashUserProfile.jsx
+++ components/UnsplashUserProfile.jsx
@@ -1,22 +1,32 @@
 import Image from 'next/image';
+import Link from 'next/link';
 import React from 'react'
 
 const UnsplashUserProfile = ({user}) => {
+    const truncateBio = () => {
+        if (user.bio) {
+            return user.bio.substring(0, 50) + "...";
+            } else {
+                return "No bio";
+             }
+    }
+    const truncatedBio = truncateBio();
   return (
-    <div className='w-full flex justify-between items-center'>
-        <div className="flex flex-col gap-3">
-        <Image src={user.profile_image.small} width={50} height={50} alt='Unsplash user profile photo' className='rounded-full' />
-        <h1 className='text-[#0d0c22] text-[32px] font-bold'>{user.username}</h1>
-        <h1 className='text-[#0d0c22] text-[48px] font-bold'>{user.bio}</h1>
+    <div className='w-full flex flex-col md:flex-row md:justify-between gap-3 items-start px-[20px] md:px-[72px] mt-10 order-2 md:order-2'>
+        <div className="flex flex-col gap-3 w-[100%] md:w-[50%]">
+        <Image src={user.profile_image.large} width={80} height={80} alt='Unsplash user profile photo' className='rounded-full' />
+        <h1 className='text-[#0d0c22] text-lg md:text-[35px] font-bold'>{user.username}</h1>
+        <h1 className='text-[#0d0c22] text-2xl md:text-[45px] md:leading-[56px] font-bold'>{truncatedBio}</h1>
         <div className="flex items-center gap-4">
-            <p className='text-[#6e6d7a] text-[16px] leading-[28px]'>{user.followers_count} followers</p>
-            <p className='text-[#6e6d7a] text-[16px] leading-[28px]'>{user.following_count} following</p>
-            <p className='text-[#6e6d7a] text-[16px] leading-[28px]'>{user.total_likes} likes</p>
-            <p className='text-[#6e6d7a] text-[16px] leading-[28px]'>{user.total_photos} photos</p>
+            <p className='text-[#6e6d7a] text-[14px] leading-[28px]'>{user.followers_count} followers</p>
+            <p className='text-[#6e6d7a] text-[14px] leading-[28px]'>{user.following_count} following</p>
+            <p className='text-[#6e6d7a] text-[14px] leading-[28px]'>{user.total_likes} likes</p>
+            <p className='text-[#6e6d7a] text-[14px] leading-[28px]'>{user.total_photos} photos</p>
         </div>
+        <Link href={user.links.html} className='bg-[#0d0c22] rounded-full font-medium text-[14px] text-white px-3 py-2 w-auto'>View unsplash profile</Link>
         </div>
-        <div className="w-[545px] h-[408.75px] object-contain rounded-2xl overflow-hidden">
-        <img src={user.profile_image.small}  alt='Unsplash user profile photo' className='' />
+        <div className="w-full md:w-[450px] h-[300px] object-contain rounded-3xl md:rounded-3xl overflow-hidden md:order-2">
+        <img src={user.photos[0].urls.full}  alt={user.photos[1].slug} className='' />
         </div>
     </div>
   );
